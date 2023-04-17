089ffecd3032dc9b4e7c706728a96eee83641339
Priller Patrick <prillerpatrick@gmail.com>
2023-04-17 15:10:33 +0200

    added testmethods

--- test/plotter/DerivationTest.java
+++ test/plotter/DerivationTest.java
@@ -6,16 +6,20 @@ import org.apache.commons.math3.analysis.derivative.DerivativeStructure;
 import org.apache.commons.math3.analysis.function.Sigmoid;
 import org.apache.commons.math3.analysis.function.Sin;
 */
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 //import org.junit.Assert.assertEquals;
 
 public class DerivationTest {
     @Test
+    @DisplayName("Derivative Sigmoid Function")
     public void testSigmoidDerivative() {
+
     }
 
     @Test
+    @DisplayName("Derivative Sinus Function")
     public void testSinDerivative() {
 
     }
