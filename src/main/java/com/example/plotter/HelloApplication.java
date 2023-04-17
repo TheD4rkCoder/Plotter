a2871799c337354f09ddfe77b75685ed2b6a17f9
”TheD4rkCodergit config --global user.name ”TheD4rkCoder <david2mairhofer@gmail.com>
2023-04-17 12:04:55 +0200

    initial commit

--- src/main/java/com/example/plotter/HelloApplication.java
+++ src/main/java/com/example/plotter/HelloApplication.java
@@ -2,19 +2,40 @@ package com.example.plotter;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class HelloApplication extends Application {
+    ArrayList<Point> points = new ArrayList<>();
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Group plotArea = new Group();
+
+
+        Group group = new Group(plotArea);
+        Scene scene = new Scene(group, 320, 240);
+        stage.setTitle("Plotter");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void drawFunction() {
+        Point maxPoint = Collections.max(points, new PointsComp());
+        Point minPoint = Collections.min(points, new PointsComp());
+        double minToMaxDifference = maxPoint.y - minPoint.y;
+
+        for (int i = 0; i < points.size() - 1; i++) {
+
+        }
     }
 
     public static void main(String[] args) {
---
+++ test/plotter/ExceptionsTest.java
@@ -0,0 +1,4 @@
+package plotter;
+
+public class ExceptionsTest {
+}
