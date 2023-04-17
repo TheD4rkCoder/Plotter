5eb9cb8dc3e01397d7fa804e0926a3932643cac6
”TheD4rkCodergit config --global user.name ”TheD4rkCoder <david2mairhofer@gmail.com>
2023-04-17 12:16:17 +0200

    initial commit

--- test/plotter/ConstantsAndFunctionsTest.java
+++ test/plotter/ConstantsAndFunctionsTest.java
@@ -1,4 +1,14 @@
 package plotter;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ConstantsAndFunctionsTest {
+    @Test
+    @DisplayName("Test für Wert von PI")
+    public void piTest() {
+        assertEquals(Math.PI, Mparser.calculate("PI"));
+    }
 }
