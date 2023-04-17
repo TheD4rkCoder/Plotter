7dc9f446d190c5c97b21884d0723c60ace0c810b
Priller Patrick <prillerpatrick@gmail.com>
2023-04-17 13:03:23 +0200

    added testmethods

--- test/plotter/DerivationTest.java
+++ test/plotter/DerivationTest.java
@@ -1,4 +1,22 @@
 package plotter;
 
+/*
+import org.apache.commons.math3.analysis.DifferentiableUnivariateFunction;
+import org.apache.commons.math3.analysis.derivative.DerivativeStructure;
+import org.apache.commons.math3.analysis.function.Sigmoid;
+import org.apache.commons.math3.analysis.function.Sin;
+*/
+import org.junit.jupiter.api.Test;
+
+//import org.junit.Assert.assertEquals;
+
 public class DerivationTest {
+    @Test
+    public void testSigmoidDerivative() {
+    }
+
+    @Test
+    public void testSinDerivative() {
+
+    }
 }
