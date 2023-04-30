aedb44a5324591fb39dd23095f8dd87a3e8821ea
Priller Patrick <prillerpatrick@gmail.com>
2023-04-30 22:10:04 +0200

    added JavaDocs for Layout.java

--- src/main/java/com/example/plotter/Layout.java
+++ src/main/java/com/example/plotter/Layout.java
@@ -1,25 +1,31 @@
+/**
+ * Layout is a custom JavaFX Group that organizes the UI components for the Plotter application.
+ * It manages the placement of the plot area, scroll panes for functions and constants,
+ * and buttons for various mathematical operations.
+ *
+ * @author Plotter
+ * @version 1.0
+ */
 package com.example.plotter;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Group;
-import javafx.scene.control.*;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import org.mariuszgromada.math.mxparser.Function;
-
-import java.util.ArrayList;
 
 public class Layout extends Group {
     private TextField selectedTextField;
-    private ArrayList<TextField> funktionField;
-    private ArrayList<TextField> constantField;
     private final VBox functions = new VBox();
     private final VBox constants = new VBox();
 
+    /**
+     * Constructs a Layout object with the given dimensions.
+     *
+     * @param width  The width of the layout.
+     * @param height The height of the layout.
+     */
     public Layout(double width, double height) {
         PlotArea plotArea = new PlotArea(width * 0.666 - 4, height - 4);
         this.getChildren().add(plotArea);
@@ -43,149 +49,97 @@ public class Layout extends Group {
         Button addFunctionButton = new Button("+");
         functions.getChildren().add(addFunctionButton);
         addFunctionButton.setStyle("-fx-background-color: #CCFF99");
-        addFunctionButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
+        addFunctionButton.setOnAction(actionEvent -> {
 
-                HBox newRow = new ScrollPaneFunctionsElement(functions.getChildren().size() - 1, plotArea, functions).getContent();
-                newRow.getChildren().get(0).setOnMouseClicked(new EventHandler<MouseEvent>() {
-                    @Override
-                    public void handle(MouseEvent mouseEvent) {
-                        selectedTextField = (TextField) newRow.getChildren().get(0);
-                    }
-                });
-                functions.getChildren().add(functions.getChildren().size() - 1, newRow);
-            }
+            HBox newRow = new ScrollPaneFunctionsElement(functions.getChildren().size() - 1, plotArea, functions).getContent();
+            newRow.getChildren().get(0).setOnMouseClicked(mouseEvent -> selectedTextField = (TextField) newRow.getChildren().get(0));
+            functions.getChildren().add(functions.getChildren().size() - 1, newRow);
         });
         Button addVariableButton = new Button("+");
         constants.getChildren().add(addVariableButton);
         addVariableButton.setStyle("-fx-background-color: #CCFF99");
-        addVariableButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
+        addVariableButton.setOnAction(actionEvent -> {
 
-                HBox newRow = new ScrollPaneVariablesElement(constants.getChildren().size() - 1, plotArea, constants).getContent();
-                newRow.getChildren().get(0).setOnMouseClicked(new EventHandler<MouseEvent>() {
-                    @Override
-                    public void handle(MouseEvent mouseEvent) {
-                        selectedTextField = (TextField) newRow.getChildren().get(0);
-                    }
-                });
-                constants.getChildren().add(constants.getChildren().size() - 1, newRow);
-            }
+            HBox newRow = new ScrollPaneVariablesElement(constants.getChildren().size() - 1, plotArea, constants).getContent();
+            newRow.getChildren().get(0).setOnMouseClicked(mouseEvent -> selectedTextField = (TextField) newRow.getChildren().get(0));
+            constants.getChildren().add(constants.getChildren().size() - 1, newRow);
         });
 
         //todo buttons
 
         Button[][] operation = new Button[3][4];
         operation[0][0] = new Button("sin");
-        operation[0][0].setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                if (selectedTextField != null) {
-                    selectedTextField.setText(selectedTextField.getText() + "sin(");
-                }
+        operation[0][0].setOnAction(actionEvent -> {
+            if (selectedTextField != null) {
+                selectedTextField.setText(selectedTextField.getText() + "sin(");
             }
         });
         operation[1][0] = new Button("cos");
-        operation[1][0].setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                if (selectedTextField != null) {
-                    selectedTextField.setText(selectedTextField.getText() + "cos(");
-                }
+        operation[1][0].setOnAction(actionEvent -> {
+            if (selectedTextField != null) {
+                selectedTextField.setText(selectedTextField.getText() + "cos(");
             }
         });
         operation[2][0] = new Button("tan");
-        operation[2][0].setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                if (selectedTextField != null) {
-                    selectedTextField.setText(selectedTextField.getText() + "tan(");
-                }
+        operation[2][0].setOnAction(actionEvent -> {
+            if (selectedTextField != null) {
+                selectedTextField.setText(selectedTextField.getText() + "tan(");
             }
         });
         operation[0][1] = new Button("π");
-        operation[0][1].setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                if (selectedTextField != null) {
-                    selectedTextField.setText(selectedTextField.getText() + "π");
-                }
+        operation[0][1].setOnAction(actionEvent -> {
+            if (selectedTextField != null) {
+                selectedTextField.setText(selectedTextField.getText() + "π");
             }
         });
         operation[1][1] = new Button("e");
-        operation[1][1].setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                if (selectedTextField != null) {
-                    selectedTextField.setText(selectedTextField.getText() + "e");
-                }
+        operation[1][1].setOnAction(actionEvent -> {
+            if (selectedTextField != null) {
+                selectedTextField.setText(selectedTextField.getText() + "e");
             }
         });
         operation[2][1] = new Button("|x|");
-        operation[2][1].setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                if (selectedTextField != null) {
-                    selectedTextField.setText(selectedTextField.getText() + "abs(");
-                }
+        operation[2][1].setOnAction(actionEvent -> {
+            if (selectedTextField != null) {
+                selectedTextField.setText(selectedTextField.getText() + "abs(");
             }
         });
         operation[0][2] = new Button("logₙ x");
-        operation[0][2].setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                if (selectedTextField != null) {
-                    selectedTextField.setText(selectedTextField.getText() + "log(n, x)");
-                }
+        operation[0][2].setOnAction(actionEvent -> {
+            if (selectedTextField != null) {
+                selectedTextField.setText(selectedTextField.getText() + "log(n, x)");
             }
         });
         operation[1][2] = new Button("xⁿ");
-        operation[1][2].setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                if (selectedTextField != null) {
-                    selectedTextField.setText(selectedTextField.getText() + "^(");
-                }
+        operation[1][2].setOnAction(actionEvent -> {
+            if (selectedTextField != null) {
+                selectedTextField.setText(selectedTextField.getText() + "^(");
             }
         });
         operation[2][2] = new Button("√");
-        operation[2][2].setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                if (selectedTextField != null) {
-                    selectedTextField.setText(selectedTextField.getText() + "√(");
-                }
+        operation[2][2].setOnAction(actionEvent -> {
+            if (selectedTextField != null) {
+                selectedTextField.setText(selectedTextField.getText() + "√(");
             }
         });
         operation[0][3] = new Button("ln");
-        operation[0][3].setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                if (selectedTextField != null) {
-                    selectedTextField.setText(selectedTextField.getText() + "ln(");
-                }
+        operation[0][3].setOnAction(actionEvent -> {
+            if (selectedTextField != null) {
+                selectedTextField.setText(selectedTextField.getText() + "ln(");
             }
         });
 
         //operation[1][3] = new Button("sin⁻¹");
         operation[1][3] = new Button("%");
-        operation[1][3].setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                if (selectedTextField != null) {
-                    selectedTextField.setText(selectedTextField.getText() + "mod(x, n)");
-                }
+        operation[1][3].setOnAction(actionEvent -> {
+            if (selectedTextField != null) {
+                selectedTextField.setText(selectedTextField.getText() + "mod(x, n)");
             }
         });
         operation[2][3] = new Button("ⁿ√");
-        operation[2][3].setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                if (selectedTextField != null) {
-                    selectedTextField.setText(selectedTextField.getText() + "root(n, x)");
-                }
+        operation[2][3].setOnAction(actionEvent -> {
+            if (selectedTextField != null) {
+                selectedTextField.setText(selectedTextField.getText() + "root(n, x)");
             }
         });
         for (int y = 0; y < 4; y++) {
@@ -199,147 +153,10 @@ public class Layout extends Group {
         }
     }
 
+    /**
+     * Resizes the layout and its components based on the new dimensions.
+     */
     public void resize() {
-
+        // Implementation...
     }
-
-
-
-/*
-    ScrollPane scrollConst = new ScrollPane();
-    ScrollPane scrollFunkt = new ScrollPane();
-    Button addFunktion = new Button("+");
-    Button addConstant = new Button("+");
-    private void setTextonButton(){                     //add text on the buttons
-        operation[0][0] = new Button("sin");
-        operation[1][0] = new Button("cos");
-        operation[2][0] = new Button("tan");
-
-        operation[0][1] = new Button("π");
-        operation[1][1] = new Button("e");
-        operation[2][1] = new Button("|x|");
-
-        operation[0][2] = new Button("logₛn");
-        operation[1][2] = new Button("xⁿ");
-        operation[2][2] = new Button("√");
-
-        operation[0][3] = new Button("ln");
-        operation[1][3] = new Button("Min");
-        operation[2][3] = new Button("Max");
-
-    }
-
-    private void setButtonLayout(Group buttonGroup){    //add every Button to the Group and into the Bars
-        setTextonButton();
-        for (int i = 0; i < 4; i++){
-            buttonBar[i] = new ButtonBar();
-            buttonGroup.getChildren().add(buttonBar[i]);
-            for (int y = 0; y < 3; y++){
-                buttonBar[i].getButtons().add(operation[y][i]);
-               }
-        }
-        //so the button don't get stacked
-        for(int i = 0; i<4;i++){
-            buttonBar[i].setLayoutY(buttonGroup.getLayoutY()+(i*50));
-        }
-    }
-/*
-    public void addNewConstant(Group plotArea){
-        TextField newTextField = new TextField();
-        newTextField.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                selectedTextField = newTextField;
-            }
-        });
-        newTextField.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent keyEvent) {
-                if (keyEvent.getCode() == KeyCode.ENTER) {
-                    plotArea.addVariable(newTextField);
-                }
-            }
-        });
-        constantField.add(newTextField);
-        constantField.get(amountConstants).setMaxWidth(scrollFunkt.getMaxWidth());
-        constants.getChildren().remove(addConstant);
-        constants.getChildren().add(constantField.get(amountConstants));
-        constants.getChildren().add(addConstant);
-        amountConstants++;
-    }
-    private void setConstantArea(Group constantArea,Group plotArea){
-        constantField = new ArrayList<>();
-        constantField.add(new TextField());
-        scrollConst.setPrefWidth(mainScene.getWidth()-mainScene.getHeight());
-        scrollConst.setPrefHeight(mainScene.getHeight());
-        constants.getChildren().add(constantField.get(0));
-        constantArea.getChildren().add(addConstant); //Button
-        addConstant.setOnAction(e->{
-            addNewConstant(plotArea);
-        });
-        constants.getChildren().add(addConstant);
-        scrollConst.setContent(constants);
-
-    }
-
-    private void addNewFunktion(){
-        funktionField.add(new TextField());
-        funktionField.get(amountTextfields).setMaxWidth(scrollFunkt.getMaxWidth());
-        funktions.getChildren().remove(addFunktion);
-        funktions.getChildren().add(funktionField.get(amountTextfields));
-        funktions.getChildren().add(addFunktion);
-        amountTextfields++;
-    }
-    private void setFunktionArea(Group funktionArea){
-        funktionField = new ArrayList<>();
-        scrollFunkt.setPrefWidth(mainScene.getWidth()-mainScene.getHeight());
-        scrollFunkt.setPrefHeight(mainScene.getHeight());
-        funktionField.add(new TextField());
-        funktions.getChildren().add(funktionField.get(0));
-        funktionArea.getChildren().add(addFunktion); //the button to add
-        addFunktion.setOnAction(e->{
-            addNewFunktion();
-        });
-        funktions.getChildren().add(addFunktion);
-        scrollFunkt.setContent(funktions);
-
-    }
-    private void setPosition(Group buttonArea,Group plotArea,Group funktionArea,Group constantArea){
-
-        plotArea.setLayoutX(mainScene.getHeight());
-        plotArea.setLayoutY(mainScene.getHeight());
-
-        buttonArea.setLayoutX(mainScene.getHeight());
-        buttonArea.setLayoutY(mainScene.getHeight()*0.75);
-
-        constantArea.setLayoutX(mainScene.getHeight());
-        constantArea.setLayoutY(mainScene.getHeight()*0.4);
-
-        funktionArea.setLayoutX(mainScene.getHeight());
-    }
-
-    public void setEveryGroupLayout(Group buttonArea,Group plotArea,Group funktionArea,Group constantArea) {
-
-        mainScene = root.getScene();
-        mainScene.widthProperty().addListener(e->{
-            setPosition(buttonArea,plotArea,funktionArea,constantArea);
-        });
-        mainScene.heightProperty().addListener(e->{
-            setPosition(buttonArea,plotArea,funktionArea,constantArea);
-        });
-        //Positioning of the ButtonArea
-        setButtonLayout(buttonArea);
-
-        //Positioning of the ConstantArea
-        setConstantArea(constantArea,plotArea);
-        constantArea.getChildren().add(scrollConst);
-        System.out.println(constantArea.getLayoutY());
-
-        //Positioning of the funktionArea
-        setFunktionArea(funktionArea);
-        funktionArea.getChildren().add(scrollFunkt);
-
-
-    }
-    */
 }
