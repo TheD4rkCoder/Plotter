050b04c7a122aec49f34323d24302e35a76d74e9
Priller Patrick <prillerpatrick@gmail.com>
2023-04-30 22:37:07 +0200

    added JavaDocs for ScrollPaneVariablesElement.java

--- src/main/java/com/example/plotter/ScrollPaneVariablesElement.java
+++ src/main/java/com/example/plotter/ScrollPaneVariablesElement.java
@@ -1,11 +1,15 @@
+/**
+ * ScrollPaneVariablesElement represents a single variable element in the scroll pane, including a TextField for input,
+ * and a button for deletion.
+ *
+ * @author Plotter
+ * @version 1.0
+ */
 package com.example.plotter;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import org.mariuszgromada.math.mxparser.Argument;
@@ -13,11 +17,18 @@ import org.mariuszgromada.math.mxparser.Argument;
 import java.util.ArrayList;
 
 public class ScrollPaneVariablesElement {
-    private static ArrayList<Integer> indexes = new ArrayList<>();
+    private static final ArrayList<Integer> indexes = new ArrayList<>();
     private int index;
     private PlotArea plotArea;
     private HBox content;
 
+    /**
+     * Constructor for creating a new ScrollPaneVariablesElement.
+     *
+     * @param index    The index of the variable in the PlotArea.
+     * @param plotArea The PlotArea to which the variable belongs.
+     * @param root     The VBox containing the elements of the ScrollPaneVariablesElement.
+     */
     public ScrollPaneVariablesElement(int index, PlotArea plotArea, VBox root) {
         indexes.add(index, index);
         this.index = index;
@@ -26,28 +37,22 @@ public class ScrollPaneVariablesElement {
         plotArea.addVariable(index, new Argument(beginArgumentName + " = 1"));
         TextField newVariableTextField = new TextField(beginArgumentName + " = 1");
         newVariableTextField.setPrefWidth(plotArea.getWidth() * 0.49 - 30);
-        newVariableTextField.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent keyEvent) {
-                if (keyEvent.getCode() == KeyCode.ENTER) {
-                    Argument a = new Argument(newVariableTextField.getText());
-                    if (!a.checkSyntax()) {
-                        return;
-                    }
-                    plotArea.removeVariable(indexes.get(index));
-                    plotArea.addVariable(indexes.get(index), a);
+        newVariableTextField.setOnKeyPressed(keyEvent -> {
+            if (keyEvent.getCode() == KeyCode.ENTER) {
+                Argument a = new Argument(newVariableTextField.getText());
+                if (!a.checkSyntax()) {
+                    return;
                 }
+                plotArea.removeVariable(indexes.get(index));
+                plotArea.addVariable(indexes.get(index), a);
             }
         });
         Button deleteButton = new Button("\uD83D\uDDD1");
-        deleteButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                root.getChildren().remove(content);
-                plotArea.removeVariable(indexes.get(index));
-                for (int i = index; i < indexes.size(); i++) {
-                    indexes.set(i, indexes.get(i) - 1);
-                }
+        deleteButton.setOnAction(actionEvent -> {
+            root.getChildren().remove(content);
+            plotArea.removeVariable(indexes.get(index));
+            for (int i = index; i < indexes.size(); i++) {
+                indexes.set(i, indexes.get(i) - 1);
             }
         });
         content = new HBox(newVariableTextField, deleteButton);
@@ -55,6 +60,11 @@ public class ScrollPaneVariablesElement {
 
     }
 
+    /**
+     * Returns the HBox containing the elements of the ScrollPaneVariablesElement.
+     *
+     * @return The HBox containing the TextField and delete button for the ScrollPaneVariablesElement.
+     */
     public HBox getContent() {
         return content;
     }
