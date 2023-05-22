9076eaee2a1242890ae1bd7fa95803cbe500d989
Priller Patrick <104975754+solotov-val@users.noreply.github.com>
2023-05-22 10:23:47 +0200

    Add files via upload

---
+++ JavaDOCS/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"com.example.plotter"}];updateSearchResults();
---
+++ JavaDOCS/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.example.plotter","l":"com.example.plotter"}];updateSearchResults();
